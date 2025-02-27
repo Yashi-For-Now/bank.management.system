d959a08f2de652c5258e9356785fb52f8a793672
Yashi-For-Now <yashisiit1@gmail.com>
2025-02-27 13:00:12 +0530

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Login extends JFrame{
+    Login(){
+        setSize(800, 480);
+        setVisible(true);
+        setLocation(350, 200);
+    }
+    public static void main(String args[]) {
+        new Login();
+    }
+}
